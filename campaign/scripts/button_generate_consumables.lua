f7cd7a84975f4b84178f4198e12ba49e0127d13b
Wil Thieme <wil.thieme@protonmail.com>
2021-08-21 22:05:07 -0400

    Update button_generate_consumables.lua

--- campaign/scripts/button_generate_consumables.lua
+++ campaign/scripts/button_generate_consumables.lua
@@ -73,49 +73,73 @@ local aPotionLevelCosts = {
 				}
 		}
 
+local function getAuraString(nCL, sSchool)
+	local sAuraStrength
+	if nCL > 9 then
+		sAuraStrength = "Overwhelming "
+	elseif nCL > 6 then
+		sAuraStrength = "Strong "
+	elseif nCL > 3 then
+		sAuraStrength = "Moderate "
+	elseif nCL > 0 then
+		sAuraStrength = "Faint "
+	else
+		sAuraStrength = ""
+	end
+
+	return sAuraStrength .. sSchool
+end
+
 function onButtonPress()
 	local nodeSpell = window.getDatabaseNode();
 
 	local sSpellName = DB.getValue(nodeSpell, "name", "");
 	local sType = window.type.getValue() or "";
-	local sClass = window.class.getValue() or "";
+	local sClass = string.lower(window.class.getValue() or "");
 	local nCL = window.cl.getValue() or 0;
 	if (not sClass or sClass == "") or (not sType or sType == "") or (not sSpellName or sSpellName == "") then
 		return
 	end
 
 	local sItemName = sType .. " of " .. sSpellName;
-	
-	local sCost, sDesc
-	local nSpellLevel = DB.getValue(nodeSpell, "level", ""):match("")
-	if sType == "Wand" then
-		sDesc = "A wand is a thin baton that contains a single spell of 4th level or lower. A wand has 50 charges when created—each charge allows the use of the wand’s spell one time. A wand that runs out of charges is just a stick."
-		Debug.chat(sClass, nSpellLevel)
-		sCost = aWandLevelCosts[sClass][nSpellLevel]
-		local nCharges = window.charges.getValue() or 0;
-		if nCharges ~= 0 then
-			sItemName = sItemName .. " [" .. nCharges .. " charges]";
-		end
-	elseif sType == "Scroll" then
-		sDesc = "A scroll is a spell (or collection of spells) that has been stored in written form. A spell on a scroll can be used only once. The writing vanishes from the scroll when the spell is activated. Using a scroll is basically like casting a spell."
-		sCost = aScrollLevelCosts[sClass][nSpellLevel]
-	else
-		sDesc = "A potion is a magic liquid that produces its effect when imbibed. Potions vary incredibly in appearance. Magic oils are similar to potions, except that oils are applied externally rather than imbibed. A potion or oil can be used only once. It can duplicate the effect of a spell of up to 3rd level that has a casting time of less than 1 minute and targets one or more creatures or objects."
-		sCost = aPotionLevelCosts[sClass][nSpellLevel]
-	end
 
-	local nodeItem = DB.findNode("item").createChild();
-	DB.setValue(nodeItem, "locked", "number", 1);
-	DB.setValue(nodeItem, "name", "string", sItemName);
-	DB.setValue(nodeItem, "nonid_name", "string", "Magic " .. sType);
-	DB.setValue(nodeItem, "type", "string", sType);
-	DB.setValue(nodeItem, "cl", "number", nCL);
-	DB.setValue(nodeItem, "description", "formattedtext", sDesc);
-	local sSchool = DB.getValue(nodeSpell, "school", ""):match("(%a+).*");
-	if sSchool and sSchool == "" then
-		DB.setValue(nodeItem, "aura", "string", sSchool);
-	end
-	if sCost and sCost ~= "" then
-		DB.setValue(nodeItem, "cost", "string", sCost .. " gp");
+	local sCost, sDesc
+	local nSpellLevel = tonumber(string.lower(DB.getValue(nodeSpell, "level", "")):match(sClass .. "%w*(%d+).*") or -1);
+	Debug.chat(sClass, nSpellLevel)
+	if nSpellLevel and nSpellLevel ~= -1 then
+		if sType == "Wand" then
+			sDesc = "A wand is a thin baton that contains a single spell of 4th level or lower. A wand has 50 charges when created—each charge allows the use of the wand’s spell one time. A wand that runs out of charges is just a stick."
+			sCost = aWandLevelCosts[sClass][nSpellLevel]
+			local nCharges = window.charges.getValue() or 0;
+			if nCharges ~= 0 then
+				sItemName = sItemName .. " [" .. nCharges .. " charges]";
+			end
+		elseif sType == "Scroll" then
+			sDesc = "A scroll is a spell (or collection of spells) that has been stored in written form. A spell on a scroll can be used only once. The writing vanishes from the scroll when the spell is activated. Using a scroll is basically like casting a spell."
+			sCost = aScrollLevelCosts[sClass][nSpellLevel]
+		else
+			sDesc = "A potion is a magic liquid that produces its effect when imbibed. Potions vary incredibly in appearance. Magic oils are similar to potions, except that oils are applied externally rather than imbibed. A potion or oil can be used only once. It can duplicate the effect of a spell of up to 3rd level that has a casting time of less than 1 minute and targets one or more creatures or objects."
+			sCost = aPotionLevelCosts[sClass][nSpellLevel]
+		end
+
+		local nodeItem = DB.findNode("item").createChild();
+		DB.setValue(nodeItem, "locked", "number", 1);
+		DB.setValue(nodeItem, "name", "string", sItemName);
+		DB.setValue(nodeItem, "nonid_name", "string", "Magic " .. sType);
+		DB.setValue(nodeItem, "type", "string", sType);
+		DB.setValue(nodeItem, "cl", "number", nCL);
+		DB.setValue(nodeItem, "description", "formattedtext", sDesc);
+		local sSchool = string.lower(DB.getValue(nodeSpell, "school", ""):match("(%a+).*"));
+		if sSchool and sSchool == "" then
+			sSchool = getAuraString(nCL, sSchool)
+			DB.setValue(nodeItem, "aura", "string", sSchool);
+		end
+		if sCost and sCost ~= "" then
+			DB.setValue(nodeItem, "cost", "string", sCost .. " gp");
+		end
+		Interface.openWindow("item", nodeItem);
+		window.close();
+	else
+		ChatManager.SystemMessage(Interface.getString('magic_item_gen_error_9'));
 	end
 end
