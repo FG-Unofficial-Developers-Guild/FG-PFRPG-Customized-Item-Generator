302d2e0ec1f4d85c97625cf26aa67cc2c5d33358
Wil Thieme <wil.thieme@protonmail.com>
2021-08-21 22:43:14 -0400

    Update record_spell.xml

--- campaign/scripts/button_generate_consumables.lua
+++ campaign/scripts/button_generate_consumables.lua
@@ -3,21 +3,22 @@
 -- attribution and copyright information.
 --
 
+-- arrays of standard gp costs keyed to the appropriate spell level 
 local aWandLevelCosts = {
 			["cleric"] = {
-					[0] = 375,[1] = 750, [2] = 4500, [3] = 11250, [4] = 21000
+					[0] = 375, [1] = 750, [2] = 4500, [3] = 11250, [4] = 21000
 				},
 			["druid"] = {
-					[0] = 375,[1] = 750, [2] = 4500, [3] = 11250, [4] = 21000
+					[0] = 375, [1] = 750, [2] = 4500, [3] = 11250, [4] = 21000
 				},
 			["wizard"] = {
-					[0] = 375,[1] = 750, [2] = 4500, [3] = 11250, [4] = 21000
+					[0] = 375, [1] = 750, [2] = 4500, [3] = 11250, [4] = 21000
 				},
 			["sorcerer"] = {
-					[0] = 375,[1] = 750, [2] = 6000, [3] = 13500, [4] = 24000
+					[0] = 375, [1] = 750, [2] = 6000, [3] = 13500, [4] = 24000
 				},
 			["bard"] = {
-					[0] = 375,[1] = 750, [2] = 6000, [3] = 15750, [4] = 30000
+					[0] = 375, [1] = 750, [2] = 6000, [3] = 15750, [4] = 30000
 				},
 			["paladin"] = {
 					[1] = 750, [2] = 6000, [3] = 15750, [4] = 30000
@@ -25,69 +26,69 @@ local aWandLevelCosts = {
 			["ranger"] = {
 					[1] = 750, [2] = 6000, [3] = 15750, [4] = 30000
 				}
-		}
+		};
 local aScrollLevelCosts = {
 		["cleric"] = {
-					["0"] = 12.5,["1"] = 25, ["2"] = 150, ["3"] = 375, ["4"] = 700, ["5"] = 1125, ["6"] = 1650, ["7"] = 2275, ["8"] = 3000, ["9"] = 3825
+					[0] = 12.5, [1] = 25, [2] = 150, [3] = 375, [4] = 700, [5] = 1125, [6] = 1650, [7] = 2275, [8] = 3000, [9] = 3825
 				},
 		["druid"] = {
-					["0"] = 12.5,["1"] = 25, ["2"] = 150, ["3"] = 375, ["4"] = 700, ["5"] = 1125, ["6"] = 1650, ["7"] = 2275, ["8"] = 3000, ["9"] = 3825
+					[0] = 12.5, [1] = 25, [2] = 150, [3] = 375, [4] = 700, [5] = 1125, [6] = 1650, [7] = 2275, [8] = 3000, [9] = 3825
 				},
 		["wizard"] = {
-					["0"] = 12.5,["1"] = 25, ["2"] = 150, ["3"] = 375, ["4"] = 700, ["5"] = 1125, ["6"] = 1650, ["7"] = 2275, ["8"] = 3000, ["9"] = 3825
+					[0] = 12.5, [1] = 25, [2] = 150, [3] = 375, [4] = 700, [5] = 1125, [6] = 1650, [7] = 2275, [8] = 3000, [9] = 3825
 				},
 		["sorcerer"] = {
-					["0"] = 12.5,["1"] = 25, ["2"] = 200, ["3"] = 450, ["4"] = 800, ["5"] = 1250, ["6"] = 1800, ["7"] = 2450, ["8"] = 3200, ["9"] = 4050
+					[0] = 12.5, [1] = 25, [2] = 200, [3] = 450, [4] = 800, [5] = 1250, [6] = 1800, [7] = 2450, [8] = 3200, [9] = 4050
 				},
 		["bard"] = {
-					["0"] = 12.5,["1"] = 25, ["2"] = 200, ["3"] = 525, ["4"] = 1000, ["5"] = 1625, ["6"] = 2400
+					[0] = 12.5, [1] = 25, [2] = 200, [3] = 525, [4] = 1000, [5] = 1625, [6] = 2400
 				},
 		["paladin"] = {
-					["1"] = 25, ["2"] = 200, ["3"] = 525, ["4"] = 1000
+					[1] = 25, [2] = 200, [3] = 525, [4] = 1000
 				},
 		["ranger"] = {
-					["1"] = 25, ["2"] = 200, ["3"] = 525, ["4"] = 1000
+					[1] = 25, [2] = 200, [3] = 525, [4] = 1000
 				}
-		}
+		};
 local aPotionLevelCosts = {
 			["cleric"] = {
-					["0"] = 25,["1"] = 50, ["2"] = 300, ["3"] = 750
+					[0] = 25, [1] = 50, [2] = 300, [3] = 750
 				},
 			["druid"] = {
-					["0"] = 25,["1"] = 50, ["2"] = 300, ["3"] = 750
+					[0] = 25, [1] = 50, [2] = 300, [3] = 750
 				},
 			["wizard"] = {
-					["0"] = 25,["1"] = 50, ["2"] = 300, ["3"] = 750
+					[0] = 25, [1] = 50, [2] = 300, [3] = 750
 				},
 			["sorcerer"] = {
-					["0"] = 25,["1"] = 50, ["2"] = 400, ["3"] = 900
+					[0] = 25, [1] = 50, [2] = 400, [3] = 900
 				},
 			["bard"] = {
-					["0"] = 25,["1"] = 50, ["2"] = 400, ["3"] = 1050
+					[0] = 25, [1] = 50, [2] = 400, [3] = 1050
 				},
 			["paladin"] = {
-					["1"] = 50, ["2"] = 400, ["3"] = 1050
+					[1] = 50, [2] = 400, [3] = 1050
 				},
 			["ranger"] = {
-					["1"] = 50, ["2"] = 400, ["3"] = 1050
+					[1] = 50, [2] = 400, [3] = 1050
 				}
-		}
+		};
 
 local function getAuraString(nCL, sSchool)
-	local sAuraStrength
+	local sAuraStrength;
 	if nCL > 9 then
-		sAuraStrength = "Overwhelming "
+		sAuraStrength = "Overwhelming ";
 	elseif nCL > 6 then
-		sAuraStrength = "Strong "
+		sAuraStrength = "Strong ";
 	elseif nCL > 3 then
-		sAuraStrength = "Moderate "
+		sAuraStrength = "Moderate ";
 	elseif nCL > 0 then
-		sAuraStrength = "Faint "
+		sAuraStrength = "Faint ";
 	else
-		sAuraStrength = ""
+		sAuraStrength = "";
 	end
 
-	return sAuraStrength .. sSchool
+	return sAuraStrength .. sSchool;
 end
 
 function onButtonPress()
@@ -98,27 +99,39 @@ function onButtonPress()
 	local sClass = string.lower(window.class.getValue() or "");
 	local nCL = window.cl.getValue() or 0;
 	if (not sClass or sClass == "") or (not sType or sType == "") or (not sSpellName or sSpellName == "") then
-		return
+		return;
 	end
 
 	local sItemName = sType .. " of " .. sSpellName;
 
-	local sCost, sDesc
+	local nCost, sDesc
 	local nSpellLevel = tonumber(string.lower(DB.getValue(nodeSpell, "level", "")):match(".*" .. sClass .. " (%d+).*") or -1);
 	if nSpellLevel and nSpellLevel ~= -1 then
 		if sType == "Wand" then
-			sDesc = "A wand is a thin baton that contains a single spell of 4th level or lower. A wand has 50 charges when created—each charge allows the use of the wand’s spell one time. A wand that runs out of charges is just a stick."
-			sCost = aWandLevelCosts[sClass][nSpellLevel]
+			sDesc = "A wand is a thin baton that contains a single spell of 4th level or lower. A wand has 50 charges when created—each charge allows the use of the wand’s spell one time. A wand that runs out of charges is just a stick.";
+			nCost = aWandLevelCosts[sClass][nSpellLevel];
+			if nCost and nSpellLevel ~= 0 then
+				local nMinCL = (nSpellLevel * 2) - 1;
+				nCost = nCost + (750 * (nCL - nMinCL));
+			end
 			local nCharges = window.charges.getValue() or 0;
 			if nCharges ~= 0 then
 				sItemName = sItemName .. " [" .. nCharges .. " charges]";
 			end
 		elseif sType == "Scroll" then
-			sDesc = "A scroll is a spell (or collection of spells) that has been stored in written form. A spell on a scroll can be used only once. The writing vanishes from the scroll when the spell is activated. Using a scroll is basically like casting a spell."
-			sCost = aScrollLevelCosts[sClass][nSpellLevel]
+			sDesc = "A scroll is a spell (or collection of spells) that has been stored in written form. A spell on a scroll can be used only once. The writing vanishes from the scroll when the spell is activated. Using a scroll is basically like casting a spell.";
+			nCost = aScrollLevelCosts[sClass][nSpellLevel];
+			if nCost and nSpellLevel ~= 0 then
+				local nMinCL = (nSpellLevel * 2) - 1;
+				nCost = nCost + (25 * (nCL - nMinCL));
+			end
 		else
-			sDesc = "A potion is a magic liquid that produces its effect when imbibed. Potions vary incredibly in appearance. Magic oils are similar to potions, except that oils are applied externally rather than imbibed. A potion or oil can be used only once. It can duplicate the effect of a spell of up to 3rd level that has a casting time of less than 1 minute and targets one or more creatures or objects."
-			sCost = aPotionLevelCosts[sClass][nSpellLevel]
+			sDesc = "A potion is a magic liquid that produces its effect when imbibed. Potions vary incredibly in appearance. Magic oils are similar to potions, except that oils are applied externally rather than imbibed. A potion or oil can be used only once. It can duplicate the effect of a spell of up to 3rd level that has a casting time of less than 1 minute and targets one or more creatures or objects.";
+			nCost = aPotionLevelCosts[sClass][nSpellLevel];
+			if nCost and nSpellLevel ~= 0 then
+				local nMinCL = (nSpellLevel * 2) - 1;
+				nCost = nCost + (50 * (nCL - nMinCL));
+			end
 		end
 
 		local nodeItem = DB.findNode("item").createChild();
@@ -133,8 +146,8 @@ function onButtonPress()
 			sSchool = getAuraString(nCL, sSchool)
 			DB.setValue(nodeItem, "aura", "string", sSchool);
 		end
-		if sCost and sCost ~= "" then
-			DB.setValue(nodeItem, "cost", "string", sCost .. " gp");
+		if nCost and nCost ~= 0 then
+			DB.setValue(nodeItem, "cost", "string", nCost .. " gp");
 		end
 		Interface.openWindow("item", nodeItem);
 		window.close();
