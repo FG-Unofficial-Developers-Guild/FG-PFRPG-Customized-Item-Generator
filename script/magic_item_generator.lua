192ec580385793438ed71c07366c917e51b4c156
Wil Thieme <wil.thieme@protonmail.com>
2021-10-18 21:16:29 -0400

    Update magic_item_generator.lua

--- script/magic_item_generator.lua
+++ script/magic_item_generator.lua
@@ -26,20 +26,20 @@ aSpecialMaterials = {
 		sStringName			= "blood crystal",
 		bMeleeWeapon		= true,
 		bAmmunition			= true,
-		sAddDescription		= "",
+		sAddDescription		= "<p>Mysterious radiation deep below the surface of the earth warps once-ordinary quartz into bloodcraving stone. If an attack with a piercing or slashing blood crystal weapon hits a target suffering from a bleed effect, the creature takes 1 additional point of damage from the attack as the blood crystal drains blood from the wound. This applies even if the creature was taking bleed damage before the attack with the blood crystal weapon. This does not increase the amount of the bleed effect.</p><p>Unfed blood crystal has a pale pink hue, darkening toward deep crimson as it becomes saturated with blood. Piercing or slashing weapons composed entirely or partially of metal can be made from blood crystal. Unworked blood crystal has a value of 500 gp per pound. Weapons made with blood crystal have one-half the normal hit points. Armor and shields cannot be made of blood crystal, as they would feed on the wearer’s own wounds.</p>",
 	},
 	["Cold iron"] = {
 		sStringName			= "cold iron",
 		bMeleeWeapon		= true,
 		bAmmunition			= true,
-		sAddDescription		= "",
+		sAddDescription		= "<p>This iron, mined deep underground and known for its effectiveness against demons and fey creatures, is forged at a lower temperature to preserve its delicate properties. Weapons made of cold iron cost twice as much to make as their normal counterparts. Also, adding any magical enhancements to a cold iron weapon increases its price by 2,000 gp. This increase is applied the first time the item is enhanced, not once per ability added.</p><p>Items without metal parts cannot be made from cold iron. An arrow could be made of cold iron, but a quarterstaff could not. A double weapon with one cold iron half costs 50% more than normal.</p>",
 	},
 	["Darkleaf cloth"] = {
 		sStringName			= "darkleaf cloth",
 		bLightArmor			= true,
 		bMediumArmor		= true,
 		bAlwaysMasterwork	= true,
-		sAddDescription		= "",
+		sAddDescription		= "<p>Darkleaf cloth is a special form of flexible material made by weaving together leaves and thin strips of bark from darkwood trees, then treating the resulting fabric with special alchemical processes. The resulting material is tough as cured hide but much lighter, making it an excellent material from which to create armor. Spell failure chances for armors made from darkleaf cloth decrease by 10% (to a minimum of 5%), maximum Dexterity bonuses increase by 2, and armor check penalties decrease by 3 (to a minimum of 0).</p><p> An item made from darkleaf cloth weighs half as much as the same item made from leather, furs, or hides. Items not primarily constructed of leather, fur, or hide are not meaningfully affected by being partially made of darkleaf cloth. As such, padded armor, leather armor, studded leather armor, and hide armor can be made out of darkleaf cloth (although other types of armor made of leather or hide might be possible). Because darkleaf cloth remains flexible, it cannot be used to construct rigid items such as shields or metal armors. Armors fashioned from darkleaf cloth are always masterwork items; the masterwork cost is included in the listed prices.</p>",
 	},
 	["Darkwood"] = {
 		sStringName			= "darkwood",
@@ -51,7 +51,7 @@ aSpecialMaterials = {
 		bRangedWeapon		= true,
 		bAmmunition			= true,
 		bAlwaysMasterwork	= true,
-		sAddDescription		= "",
+		sAddDescription		= "<p>This rare magic wood is as hard as normal wood but very light. Any wooden or mostly wooden item (such as a bow or spear) made from darkwood is considered a masterwork item and weighs only half as much as a normal wooden item of that type. Items not normally made of wood or only partially of wood (such as a battleaxe or a mace) either cannot be made from darkwood or do not gain any special benefit from being made of darkwood. The armor check penalty of a darkwood shield is lessened by 2 compared to an ordinary shield of its type. To determine the price of a darkwood item, use the original weight but add 10 gp per pound to the price of a masterwork version of that item.<p>",
 	},
 	["Dragonhide"] = {
 		sStringName			= "dragonhide",
@@ -60,14 +60,14 @@ aSpecialMaterials = {
 		bHeavyArmor			= true,
 		bShield				= true,
 		bAlwaysMasterwork	= true,
-		sAddDescription		= "",
+		sAddDescription		= "<p>Armorsmiths can work with the hides of dragons to produce armor or shields of masterwork quality. One dragon produces enough hide to make a single suit of masterwork hide armor for a creature one size category smaller than the dragon. By selecting only choice scales and bits of hide, an armorsmith can produce one suit of masterwork banded mail for a creature two sizes smaller, one suit of masterwork half-plate for a creature three sizes smaller, or one masterwork breastplate or suit of full plate for a creature four sizes smaller. In each case, enough hide is available to produce a light or heavy masterwork shield in addition to the armor, provided that the dragon is Large or larger. If the dragonhide comes from a dragon that had immunity to an energy type, the armor is also immune to that energy type, although this does not confer any protection to the wearer. If the armor or shield is later given the ability to protect the wearer against that energy type, the cost to add such protection is reduced by 25%.<p><p>Because dragonhide armor isn’t made of metal, druids can wear it without penalty.</p><p>Dragonhide armor costs twice as much as masterwork armor of that type, but it takes no longer to make than ordinary armor of that type (double all Craft results).</p>",
 	},
 	["Eel hide"] = {
 		sStringName			= "eel hide",
 		bLightArmor			= true,
 		bMediumArmor		= true,
 		bAlwaysMasterwork	= true,
-		sAddDescription		= "",
+		sAddDescription		= "<p>This supple material offers as much protection as leather, but is more flexible and resistant to electricity. Leather, hide, or studded leather armor can be produced with eel hide. The armor check penalty of such armor is reduced by 1 (to a minimum of 0) and the maximum Dexterity bonus of the armor is increased by 1. Additionally, wearing eel hide grants the wearer electricity resistance 2. Armor crafted from eel hide is always considered masterwork, and the masterwork costs are included in the listed prices.</p>",
 	},
 	["Elysian bronze"] = {
 		sStringName			= "elysian bronze",
@@ -78,7 +78,7 @@ aSpecialMaterials = {
 		bMeleeWeapon		= true,
 		bRangedWeapon		= true,
 		bAmmunition			= true,
-		sAddDescription		= "",
+		sAddDescription		= "<p>First crafted in the deeps of time by the titans and bestowed as gifts to monster-slaying heroes among the lesser races, Elysian bronze retains the brazen coloration of its namesake but is as hard as steel. A weapon made of Elysian bronze adds a +1 bonus on weapon damage rolls against magical beasts and monstrous humanoids; this damage is multiplied on a critical hit. After a creature uses an Elysian bronze weapon to deal damage to a magical beast or monstrous humanoid, the wielder gains a +1 bonus on attack rolls against that specific creature type (for example, against chimeras, not all magical beasts) for the next 24 hours, or until the weapon deals damage to a different kind of magical beast or monstrous humanoid.</p><p>Armor made of Elysian bronze also protects its wearer against the natural weapons or unarmed strikes of magical beasts and monstrous humanoids, providing damage reduction as if it were adamantine (1/— for light armor, 2/— for medium armor, or 3/— for heavy armor). It does not provide this protection against creatures of other types.</p>",
 	},
 	["Fire-forged steel"] = {
 		sStringName			= "fire forged steel",
@@ -90,7 +90,7 @@ aSpecialMaterials = {
 		bRangedWeapon		= true,
 		bAmmunition			= true,
 		bAlwaysMasterwork	= true,
-		sAddDescription		= "",
+		sAddDescription		= "<p>Dwarves stumbled across the secret of crafting fire-forged steel in an effort to make forge-friendly tools. It didn’t take them long to adapt its unique properties to arms and armor. Fire-forged steel channels heat in one direction to protect its wearer or wielder. When it is crafted into armor, heat is channeled away from the wearer, offering some limited protection. Armor crafted from fire-forged steel grants the wearer fire resistance 2.</p><p>Weapons crafted from fire-forged steel similarly channel heat away from the wearer; this does not grant the wielder energy resistance. Instead, the blade absorbs and channels heat to the parts of the weapon that contact enemies. If the weapon is exposed to 10 points or more of fire damage (such as from an opponent’s fireball or by holding it in a campfire for 1 full round), the weapon adds +1d4 points of fire damage to its attacks for the next 2 rounds. If the wielder is wearing fire-forged armor and using a fire-forged weapon, this bonus damage increases to 1d6 points of fire damage and lasts for 4 rounds. This bonus damage does not stack with fire damage from weapon enhancements such as flaming.</p>",
 	},
 	["Frost-forged steel"] = {
 		sStringName			= "frost forged steel",
@@ -102,7 +102,7 @@ aSpecialMaterials = {
 		bRangedWeapon		= true,
 		bAmmunition			= true,
 		bAlwaysMasterwork	= true,
-		sAddDescription		= "",
+		sAddDescription		= "<p>This material is the same substance as fire-forged steel with a subtle difference in the alignment of the metal during crafting. Instead of channeling heat away from the wearer, it channels heat toward the wearer. Frost-forged steel works similarly to fire-forged steel, except its effects apply to cold damage rather than fire damage. This means frost-forged steel weapons are less useful than their fire-forged counterparts, as there are few nonmagical sources of cold that can quickly imbue it with enough cold energy to deal bonus damage.</p><p>Armor and weapons made from frost-forged steel are always considered masterwork, and the masterwork costs are included in the listed prices.</p>",
 	},
 	["Greenwood"] = {
 		sStringName			= "greenwood",
