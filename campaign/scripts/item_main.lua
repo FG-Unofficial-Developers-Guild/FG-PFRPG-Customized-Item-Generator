7a7fabdcef3790ad46d499bf264ca8f478f61913
Wil Thieme <wil.thieme@protonmail.com>
2022-03-24 21:06:05 -0400

    better line length and formatting

--- campaign/scripts/item_main.lua
+++ campaign/scripts/item_main.lua
@@ -1,27 +1,16 @@
 --
 -- Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
+function onInit() update(); end
 
-function onInit()
-	update();
-end
+function VisDataCleared() update(); end
 
-function VisDataCleared()
-	update();
-end
-
-function InvisDataAdded()
-	update();
-end
+function InvisDataAdded() update(); end
 
 function updateControl(sControl, bReadOnly, bID)
-	if not self[sControl] then
-		return false;
-	end
+	if not self[sControl] then return false; end
 
-	if not bID then
-		return self[sControl].update(bReadOnly, true);
-	end
+	if not bID then return self[sControl].update(bReadOnly, true); end
 
 	return self[sControl].update(bReadOnly);
 end
@@ -32,65 +21,65 @@ function update()
 	local bID, bOptionID = ItemManager.getIDState(nodeRecord);
 
 	local sType = string.lower(type.getValue());
-	local sSubType = string.lower(DB.getValue(nodeRecord, "subtype"));
+	local sSubType = string.lower(DB.getValue(nodeRecord, 'subtype'));
 
-	local bWeapon = (string.find(sType, "weapon") ~= nil);
-	local bArmor = (string.find(sType, "armor") ~= nil);
-	local bShield = (string.find(sSubType, "shield") ~= nil);
-	local bWand = (string.find(sType, "wand") ~= nil);
-	local bStaff = (string.find(sType, "staff") ~= nil);
-	local bWondrous = (string.find(sType, "wondrous item") ~= nil);
+	local bWeapon = (string.find(sType, 'weapon') ~= nil);
+	local bArmor = (string.find(sType, 'armor') ~= nil);
+	local bShield = (string.find(sSubType, 'shield') ~= nil);
+	local bWand = (string.find(sType, 'wand') ~= nil);
+	local bStaff = (string.find(sType, 'staff') ~= nil);
+	local bWondrous = (string.find(sType, 'wondrous item') ~= nil);
 
 	local bSection1 = false;
 	if bOptionID and Session.IsHost then
-		if updateControl("nonid_name", bReadOnly, true) then bSection1 = true; end;
+		if updateControl('nonid_name', bReadOnly, true) then bSection1 = true; end
 	else
-		updateControl("nonid_name", false);
+		updateControl('nonid_name', false);
 	end
 
 	if bOptionID and (Session.IsHost or not bID) then
-		if updateControl("nonidentified", bReadOnly, true) then bSection1 = true; end;
+		if updateControl('nonidentified', bReadOnly, true) then bSection1 = true; end
 	else
-		updateControl("nonidentified", false);
+		updateControl('nonidentified', false);
 	end
 
 	local bSection2 = false;
-	if updateControl("type", bReadOnly, bID) then bSection2 = true; end
-	if updateControl("subtype", bReadOnly, bID) then bSection2 = true; end
+	if updateControl('type', bReadOnly, bID) then bSection2 = true; end
+	if updateControl('subtype', bReadOnly, bID) then bSection2 = true; end
 
 	local bSection3 = false;
-	if updateControl("cost", bReadOnly, bID) then bSection3 = true; end
-	if updateControl("weight", bReadOnly, bID) then bSection3 = true; end
-	if updateControl("size", bReadOnly, bID) then bSection3 = true; end
+	if updateControl('cost', bReadOnly, bID) then bSection3 = true; end
+	if updateControl('weight', bReadOnly, bID) then bSection3 = true; end
+	if updateControl('size', bReadOnly, bID) then bSection3 = true; end
 
 	local bSection8 = false;
-	local bUsingACIM = StringManager.contains(Extension.getExtensions(), "FG-PFRPG-Advanced-Item-Actions")
+	local bUsingACIM = StringManager.contains(Extension.getExtensions(), 'FG-PFRPG-Advanced-Item-Actions')
 	if bUsingACIM then
-		if updateControl("damage", bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
-		if updateControl("damagetype", bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
-		if updateControl("critical", bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
-		if updateControl("range", bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
+		if updateControl('damage', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
+		if updateControl('damagetype', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
+		if updateControl('critical', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
+		if updateControl('range', bReadOnly, bID and (bWeapon or bShield)) then bSection8 = true; end
 	else
-		if updateControl("damage", bReadOnly, bID and bWeapon) then bSection4 = true; end
-		if updateControl("damagetype", bReadOnly, bID and bWeapon) then bSection4 = true; end
-		if updateControl("critical", bReadOnly, bID and bWeapon) then bSection4 = true; end
-		if updateControl("range", bReadOnly, bID and bWeapon) then bSection4 = true; end
+		if updateControl('damage', bReadOnly, bID and bWeapon) then bSection4 = true; end
+		if updateControl('damagetype', bReadOnly, bID and bWeapon) then bSection4 = true; end
+		if updateControl('critical', bReadOnly, bID and bWeapon) then bSection4 = true; end
+		if updateControl('range', bReadOnly, bID and bWeapon) then bSection4 = true; end
 	end
 
 	local bSection4 = false;
-	if updateControl("ac", bReadOnly, bID and bArmor) then bSection4 = true; end
-	if updateControl("maxstatbonus", bReadOnly, bID and bArmor) then bSection4 = true; end
-	if updateControl("checkpenalty", bReadOnly, bID and bArmor) then bSection4 = true; end
-	if updateControl("spellfailure", bReadOnly, bID and bArmor) then bSection4 = true; end
-	if updateControl("speed30", bReadOnly, bID and bArmor) then bSection4 = true; end
-	if updateControl("speed20", bReadOnly, bID and bArmor) then bSection4 = true; end
+	if updateControl('ac', bReadOnly, bID and bArmor) then bSection4 = true; end
+	if updateControl('maxstatbonus', bReadOnly, bID and bArmor) then bSection4 = true; end
+	if updateControl('checkpenalty', bReadOnly, bID and bArmor) then bSection4 = true; end
+	if updateControl('spellfailure', bReadOnly, bID and bArmor) then bSection4 = true; end
+	if updateControl('speed30', bReadOnly, bID and bArmor) then bSection4 = true; end
+	if updateControl('speed20', bReadOnly, bID and bArmor) then bSection4 = true; end
 
-	local bUsingEnhanceItems = StringManager.contains(Extension.getExtensions(), "FG-PFRPG-Enhanced-Items")
+	local bUsingEnhanceItems = StringManager.contains(Extension.getExtensions(), 'FG-PFRPG-Enhanced-Items')
 	if bUsingEnhanceItems then
 		current_label.setVisible(false);
 		maxcharges.setVisible(false);
 		maxcharges_label.setVisible(false);
-		if updateControl("charge", bReadOnly, bID and (bWand or bStaff)) then
+		if updateControl('charge', bReadOnly, bID and (bWand or bStaff)) then
 			current_label.setVisible(true);
 			maxcharges.setVisible(true);
 			maxcharges.setReadOnly(bReadOnly);
@@ -99,16 +88,16 @@ function update()
 		end
 		charge.setReadOnly(false);
 
-		if updateControl("equipslot", bReadOnly, bID and bWondrous) then bSection8 = true; end
+		if updateControl('equipslot', bReadOnly, bID and bWondrous) then bSection8 = true; end
 	end
 
-	if updateControl("properties", bReadOnly, bID and (bWeapon or bArmor)) then bSection4 = true; end
+	if updateControl('properties', bReadOnly, bID and (bWeapon or bArmor)) then bSection4 = true; end
 
 	local bSection5 = false;
-	if updateControl("bonus", bReadOnly, bID and (bWeapon or bArmor)) then bSection5 = true; end
-	if updateControl("aura", bReadOnly, bID) then bSection5 = true; end
-	if updateControl("cl", bReadOnly, bID) then bSection5 = true; end
-	if updateControl("prerequisites", bReadOnly, bID) then bSection5 = true; end
+	if updateControl('bonus', bReadOnly, bID and (bWeapon or bArmor)) then bSection5 = true; end
+	if updateControl('aura', bReadOnly, bID) then bSection5 = true; end
+	if updateControl('cl', bReadOnly, bID) then bSection5 = true; end
+	if updateControl('prerequisites', bReadOnly, bID) then bSection5 = true; end
 
 	local bSection6 = bID;
 	description.setVisible(bID);
@@ -116,18 +105,16 @@ function update()
 
 	local bSection7 = false;
 	if bUsingEnhanceItems then
-		updateControl("sourcebook", bReadOnly, bID);
+		updateControl('sourcebook', bReadOnly, bID);
 		divider6.setVisible(false);
 		gmonly_label.setVisible(false);
 		gmonly.setVisible(false);
 		if bOptionID and Session.IsHost then
-			if updateControl("gmonly", bReadOnly, true) then bSection7 = true; end
+			if updateControl('gmonly', bReadOnly, true) then bSection7 = true; end
 		elseif Session.IsHost then
-			updateControl("gmonly", bReadOnly, false);
+			updateControl('gmonly', bReadOnly, false);
 		end
-		if Session.IsHost then 
-			divider6.setVisible((bSection1 or bSection2 or bSection3 or bSection4 or bSection5) and bSection7);
-		end
+		if Session.IsHost then divider6.setVisible((bSection1 or bSection2 or bSection3 or bSection4 or bSection5) and bSection7); end
 	end
 
 	divider.setVisible(bSection1 and bSection2);
