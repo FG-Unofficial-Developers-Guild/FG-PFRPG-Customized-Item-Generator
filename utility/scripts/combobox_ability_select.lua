0d6dbea4ac7e87782653feb8ffd66ccce138293b
Wil Thieme <wil.thieme@protonmail.com>
2024-02-25 19:40:27 -0500

    move scripts to files

---
+++ utility/scripts/combobox_ability_select.lua
@@ -0,0 +1,88 @@
+--
+-- Please see the LICENSE.md file included with this distribution for attribution and copyright information.
+--
+
+-- luacheck: globals addItems add getValue
+function onInit()
+	if super and super.onInit then
+		super.onInit()
+	end
+	local nodeItem = DB.getChild(window.getDatabaseNode(), '...')
+	if not nodeItem then
+		return
+	end
+	local sType, sSubType = CustomItemGen.getItemType(nodeItem)
+	if CustomItemGen.notifyMissingTypeData(sType, sSubType) then
+		return
+	end
+
+	local aAbilityList = CustomItemGen.getAbilityList(sType, sSubType)
+
+	local dDamageType = DB.getChild(nodeItem, 'damagetype')
+	local sName = DB.getValue(nodeItem, 'name')
+
+	local bMeleeWeapon = (sType == 'Weapon' and sSubType == 'Melee')
+	local bRangedWeapon = (sType == 'Weapon' and sSubType == 'Ranged')
+	local bSlashingWeapon = false
+	local bPiercingWeapon = false
+	local bBludgeoningWeapon = false
+	if dDamageType then
+		bSlashingWeapon = dDamageType.getValue():lower():match('slashing')
+		bPiercingWeapon = dDamageType.getValue():lower():match('piercing')
+		bBludgeoningWeapon = dDamageType.getValue():lower():match('bludgeoning')
+	end
+	local bCrossbow = (sName:lower():match('crossbow'))
+	local bBow = (sName:lower():match('bow') and not bCrossbow)
+	local bFirearm = (sType == 'Firearm')
+
+	addItems({ Interface.getString('bmos_customizeitem_bonus_none') })
+
+	for key, value in pairs(aAbilityList) do
+		if bMeleeWeapon and ((bSlashingWeapon and value.bSlashing) or (bPiercingWeapon and value.bPiercing) or (bBludgeoningWeapon and value.bBludgeoning)) then
+			add(key)
+		elseif bRangedWeapon then
+			if
+				(value.bCrossbow or value.bBow or value.bFirearm)
+				and ((bCrossbow and value.bCrossbow) or (bBow and value.bBow) or (bFirearm and value.bFirearm))
+			then
+				add(key)
+			elseif not (value.bCrossbow or value.bBow or value.bFirearm or value.bThrown) then
+				add(key)
+			end
+		else
+			add(key)
+		end
+	end
+end
+
+function onValueChanged()
+	if super and super.onValueChanged then
+		super.onValueChanged()
+	end
+	local nodeItem = DB.getChild(window.getDatabaseNode(), '...')
+	if not nodeItem then
+		return
+	end
+	local sType, sSubType = CustomItemGen.getItemType(nodeItem)
+	if CustomItemGen.notifyMissingTypeData(sType, sSubType) then
+		return
+	end
+
+	local aAbilityList = CustomItemGen.getAbilityList(sType, sSubType)
+
+	local sAbility = getValue()
+
+	if aAbilityList ~= nil and next(aAbilityList[sAbility].aSubSelection) ~= nil then
+		window.ability_sub_select.clear()
+		window.ability_sub_select.addItems({ Interface.getString('bmos_customizeitem_bonus_none') })
+		for key, _ in pairs(aAbilityList[sAbility].aSubSelection) do
+			window.ability_sub_select.add(key)
+		end
+		window.ability_sub_select.setValue(Interface.getString('bmos_customizeitem_bonus_none'))
+		window.ability_sub_select.setComboBoxVisible(true)
+		window.ability_sub_select_label.setVisible(true)
+	else
+		window.ability_sub_select.setComboBoxVisible(false)
+		window.ability_sub_select_label.setVisible(false)
+	end
+end
---
+++ utility/scripts/comboboxc_material.lua
@@ -0,0 +1,43 @@
+--
+-- Please see the LICENSE.md file included with this distribution for attribution and copyright information.
+--
+
+-- luacheck: globals addItems add
+function onInit()
+	if super and super.onInit then
+		super.onInit()
+	end
+	local nodeItem = window.getDatabaseNode()
+
+	local sType, sSubType = CustomItemGen.getItemType(nodeItem)
+	if CustomItemGen.notifyMissingTypeData(sType, sSubType) then
+		return
+	end
+
+	local bLightArmor = (sType == 'armor' and sSubType == 'light')
+	local bMediumArmor = (sType == 'armor' and sSubType == 'medium')
+	local bHeavyArmor = (sType == 'armor' and sSubType == 'heavy')
+	local bShield = (sType == 'shield')
+	local bMeleeWeapon = (sType == 'weapon' and sSubType == 'melee')
+	local bRangedWeapon = (sType == 'weapon' and sSubType == 'ranged')
+	local bAmmunition = (sType == 'ammunition')
+
+	addItems({ Interface.getString('bmos_customizeitem_bonus_none') })
+	for key, value in pairs(CustomItemGenItemData.aSpecialMaterials) do
+		if bLightArmor and value.bLightArmor then
+			add(key)
+		elseif bMediumArmor and value.bMediumArmor then
+			add(key)
+		elseif bHeavyArmor and value.bHeavyArmor then
+			add(key)
+		elseif bShield and value.bShield then
+			add(key)
+		elseif bMeleeWeapon and value.bMeleeWeapon then
+			add(key)
+		elseif bRangedWeapon and value.bRangedWeapon then
+			add(key)
+		elseif bAmmunition and value.bAmmunition then
+			add(key)
+		end
+	end
+end
